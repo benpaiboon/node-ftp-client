2409b5dd3763002dd6f7ec95cb03c52c2169640f
unknown <paiboon@setscope.com>
2018-07-12 00:16:43 +0700

    add getTodayFolder() & readCSV()

--- src/index.js
+++ src/index.js
@@ -1,27 +1,62 @@
-// Init Basic FTP Dependency
-const ftp = require("basic-ftp");
-const ftpConfig = require('./config/ftp.json');
-const ftpFolder = require('./config/ftp.folder.json');
+// Init today folder
+function getTodayFolder() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //January is 0!
+  let yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
 
-// Init FTP function for download files from MPAI Server
-async function downloadFilesFromFTP() {
-  const client = new ftp.Client();
+  let dateSyntax = `${yyyy}${mm}${dd}`;
+  let todayFolder = dateSyntax - 1;
+  return todayFolder;
+}
+
+function readCSV() {
+  const fs = require('fs');
+  const csv = require('fast-csv');
+  const ftpFolder = require('./config/ftp.folder.json');
+  const todayFolder = getTodayFolder();
+  const filePath = `./${ftpFolder.localDownloadDir}/${todayFolder}/`;
+
   try {
-    // let today = new Date('July 07, 2018 23:15:30'); //Test saturday
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1; //January is 0!
-    let yyyy = today.getFullYear();
-    if (dd < 10) {
-      dd = '0' + dd;
+    fs.readdirSync(filePath).forEach(file => {
+      // console.log(file);
+      let fullPath = `${filePath}${file}`;
+      let stream = fs.createReadStream(fullPath);
+      // read csv
+      csv
+        .fromStream(stream, { headers: false })
+        .on("data", function (data) {
+          console.log(data);
+        })
+        .on("end", function () {
+          console.log("done");
+        });
+    });
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log('Directory not found!');
+    } else {
+      throw err;
     }
-    if (mm < 10) {
-      mm = '0' + mm;
-    }
+  }
+}
 
-    // Init today folder
-    let dateSyntax = `${yyyy}${mm}${dd}`;
-    let todayFolder = dateSyntax - 1;
+// Init FTP function for download files from Server
+async function downloadFilesFromFTP() {
+  // Init Basic FTP Dependency
+  const ftp = require("basic-ftp");
+  const ftpConfig = require('./config/ftp.json');
+  const ftpFolder = require('./config/ftp.folder.json');
+  const client = new ftp.Client();
+
+  try {
+    let todayFolder = getTodayFolder();
 
     // Connect FTP server
     await client.access(ftpConfig);
@@ -43,12 +78,16 @@ async function downloadFilesFromFTP() {
 
   }
   catch (err) {
-    console.log(err);
-    console.log('');
-    console.log(`*** The system can't download csv files because Merchant FTP server hasn't been updated list of data. ***`);
+    if (err.code === 550) {
+      console.log(`*** The system can't download csv files because FTP server hasn't been updated list of data. ***`);
+    } else {
+      throw err;
+    }
   }
   client.close()
 }
 
 // Fired function
 downloadFilesFromFTP();
+
+// readCSV();
